0918cd82ae3f9c5e9d560ffdfd3d2ca30ff1d43c
ankitpandey12199 <ankitpandey12199@gmail.com>
2023-08-30 22:00:46 +0530

    third and fourth pattern

---
+++ src/com/pattern_programming/Pattern3.java
@@ -0,0 +1,27 @@
+package com.pattern_programming;
+/*
+  this code prints this
+        *
+      * *
+    * * *
+  * * * *
+* * * * *
+ */
+import java.util.Scanner;
+
+public class Pattern3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n-1; j++) {
+                System.out.print("  ");
+            }
+            for (int j = 0; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+    }
+}
---
+++ src/com/pattern_programming/Pattern4.java
@@ -0,0 +1,27 @@
+package com.pattern_programming;
+/*
+  this code prints this pattern
+  * * * * * 
+    * * * *
+      * * * 
+        * * 
+          * 
+
+ */
+import java.util.Scanner;
+
+public class Pattern4 {
+    public static void main(String[] args) {
+      Scanner sc=new Scanner(System.in);
+      int n=sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print("  ");
+            }
+            for (int j = i; j < n; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
