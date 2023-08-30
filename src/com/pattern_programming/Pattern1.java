6c2872780a85b4370522261a1811248644d60f7c
ankitpandey12199 <ankitpandey12199@gmail.com>
2023-08-30 22:19:35 +0530

    fifth pattern

--- src/com/pattern_programming/Pattern1.java
+++ src/com/pattern_programming/Pattern1.java
@@ -9,7 +9,7 @@ import java.util.Scanner;
  * * * *
  * * * * *
  */
-public class patern1 {
+public class Pattern1 {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
---
+++ src/com/pattern_programming/Pattern5.java
@@ -0,0 +1,30 @@
+package com.pattern_programming;
+
+import java.util.Scanner;
+
+public class Pattern5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int m = sc.nextInt();
+        int n = m / 2;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n - 1; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = i; j < n; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
